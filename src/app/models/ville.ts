e7634d8938ec5fc8757c407b9135296d9a507c64
GitBenoitAmardeilh <amardeilhbenoit@gmail.com>
2018-07-12 09:42:03 +0200

    creation of class ville

---
+++ src/app/models/ville.ts
@@ -0,0 +1,51 @@
+export class Ville {
+
+  private _id: string;
+  private _nom: string;
+  private _longitude: string;
+  private _latitude: string;
+
+  constructor(id: string, nom: string, longitude: string, latitude: string) {
+
+      this.id = id;
+      this.nom = nom;
+      this.longitude  = longitude;
+      this.latitude = latitude;
+
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+  public set id(v: string) {
+    this._id = v;
+  }
+
+
+  public get nom(): string {
+    return this._nom;
+  }
+  public set nom(v: string) {
+    this._nom = v;
+  }
+
+
+
+  public get longitude(): string {
+    return this._longitude;
+  }
+  public set longitude(v: string) {
+    this._longitude = v;
+  }
+
+
+
+  public get latitude(): string {
+    return this._latitude;
+  }
+  public set latitude(v: string) {
+    this._latitude = v;
+  }
+
+
+}
